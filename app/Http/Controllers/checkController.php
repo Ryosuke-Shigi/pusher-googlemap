a1e32d8472988913d40b5669e9d3dde67b7c1dd6
Ryosuke-Shigi <adalovelace6365@gmail.com>
2022-02-23 15:35:19 +0900

    mobile

--- app/Http/Controllers/checkController.php
+++ app/Http/Controllers/checkController.php
@@ -28,7 +28,7 @@ class checkController extends Controller
     //コメント投稿
     public function commenter(REQUEST $request){
         event(new commented($request->comment));
-        return response()->jeson(['message'=>'コメント']);
+        return response()->json(['message'=>'コメント']);
     }
 
 }
